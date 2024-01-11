e789517946f7b60fdc9d28887f94f54ab1562ad8
Natan-Mesele <huluale12@gmail.com>
2024-01-11 21:57:52 +0300

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,31 +6,39 @@ import MenuIcon from '@material-ui/icons/Menu';
 
 function Header() {
   return (
-    <div className='p-4'>
-        <div className='flex justify-between'>
-            <div>
-                <LocationOnIcon />
-                <span>EDNA MALL</span>
-                <AccessTimeIcon />
+    <div className='px-4 py-1'>
+        <div className='flex justify-between gap-4 border-b-2 p-2'>
+            <div className='text-xs'>
+                <LocationOnIcon className='mr-2'/>
+                <span>EDNA MALL</span> 
+            </div>
+            <div className='text-xs flex-1 border-l-2 ' >    
+                <AccessTimeIcon className='mx-2'/>
                 <span>06:08 - 23:08</span>
             </div>
-            <div>
-                <SearchIcon />
+            <div> 
+                <SearchIcon className='cursor-pointer'/>
             </div>
         </div>
-        <div className='flex'>
-            <div>
+        <div className='flex justify-between items-center '>
+            <div className='w-20'>
                 <img src="https://order.qranbessa.net/uploads/a3fb7c7b6c408451a0a8bf4716711929.png" />
             </div>
-            <div>
-                <a href='#'>Menu</a>
-                <a href='#'>Reservation</a>
-                <a href='#'>Cart</a>
-                <a href='#'>Login</a>
+            <div className='hidden sm:block'>
+                <div className='flex gap-6 item-center'>
+                    <a href='#'>Menu</a>
+                    <a href='#'>Reservation</a>
+                    <a href='#'>Cart</a>
+                    <a href='#'>Login</a>
+                </div>
+                
             </div>
-            <div>
-                <MenuIcon />
+            <div className='sm:hidden'>
+                <div className='flex items-center cursor-pointer'>
+                    <MenuIcon />
+                </div>
             </div>
+            
         </div>
     </div>
   )
