f806e35fa889a32dcc9f343af7565fa6463548f9
Natan-Mesele <huluale12@gmail.com>
2024-01-12 11:58:41 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import Header from './components/Header'
+import Side from './components/Side'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Header />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+          <Routes>
+            <Route path="/" element={<Side/>}></Route>
+          </Routes>
+      </div>
+    </Router>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,23 +6,23 @@ import MenuIcon from '@material-ui/icons/Menu';
 
 function Header() {
   return (
-    <div className='px-4 py-1'>
-        <div className='flex justify-between gap-4 border-b-2 p-2'>
+    <div className='p-4 bg-black text-white'>
+        <div className='flex justify-between gap-4 border-b-2 pb-4'>
             <div className='text-xs'>
                 <LocationOnIcon className='mr-2'/>
                 <span>EDNA MALL</span> 
             </div>
             <div className='text-xs flex-1 border-l-2 ' >    
-                <AccessTimeIcon className='mx-2'/>
+                <AccessTimeIcon className='mx-3'/>
                 <span>06:08 - 23:08</span>
             </div>
-            <div> 
+            <div className='hidden sm:block'> 
                 <SearchIcon className='cursor-pointer'/>
             </div>
         </div>
-        <div className='flex justify-between items-center '>
-            <div className='w-20'>
-                <img src="https://order.qranbessa.net/uploads/a3fb7c7b6c408451a0a8bf4716711929.png" />
+        <div className='flex justify-between items-center pt-4'>
+            <div className=''>
+                <img src="https://order.qranbessa.net/uploads/a3fb7c7b6c408451a0a8bf4716711929.png" className='rounded-lg object-cover h-20 w-22'/>
             </div>
             <div className='hidden sm:block'>
                 <div className='flex gap-6 item-center uppercase text-sm'>
