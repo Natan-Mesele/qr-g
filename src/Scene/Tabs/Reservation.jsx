b39cabf169f970606e9a28df045acdf9b3403db8
Natan-Mesele <huluale12@gmail.com>
2024-01-18 21:01:55 +0300

    reservation

--- src/Scene/Tabs/Reservation.jsx
+++ src/Scene/Tabs/Reservation.jsx
@@ -1,6 +1,23 @@
 import React, { useState } from 'react';
+import CustomSelector from '../../components/CustomSelector';
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  TextField,
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  Checkbox,
+  ListItemText,
+  OutlinedInput,
+} from "@material-ui/core";
 
 const Reservation = ({ passFormData }) => {
+  const [selectedOption, setSelectedOption] = useState('');
   const [formData, setFormData] = useState({
     name: '',
     phone: '',
@@ -8,6 +25,10 @@ const Reservation = ({ passFormData }) => {
     time: '',
     message: ''
   });
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
 
   const handleChange = (e) => {
     setFormData({
@@ -23,79 +44,84 @@ const Reservation = ({ passFormData }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="max-w-md mx-auto mt-8 mb-4 p-4 border rounded">
-      <div className="mb-4">
-        <div className='text-center mt-2 mb-8'>
-          <h1 className='text-md font-bold mb-1'>TABLE RESERVATION</h1>
-          <span className='text-md text-gray-500'>Make table reservation at QRAnbessa.</span>
-        </div>  
-        <label htmlFor="name" className="block text-sm font-medium text-gray-600">
-          Name:
-        </label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          className="mt-1 p-2 w-full border rounded-md"
-        />
-      </div>
-      <div className="mb-4">
-        <label htmlFor="phone" className="block text-sm font-medium text-gray-600">
-          Phone:
-        </label>
-        <input
-          type="text"
-          id="phone"
-          name="phone"
-          value={formData.phone}
-          onChange={handleChange}
-          className="mt-1 p-2 w-full border rounded-md"
-        />
-      </div>
-      <div className="mb-4">
-        <label htmlFor="date" className="block text-sm font-medium text-gray-600">
-          Date:
-        </label>
-        <input
-          type="date"
-          id="date"
-          name="date"
-          value={formData.date}
-          onChange={handleChange}
-          className="mt-1 p-2 w-full border rounded-md"
-        />
-      </div>
-      <div className="mb-4">
-        <label htmlFor="time" className="block text-sm font-medium text-gray-600">
-          Time:
-        </label>
-        <input
-          type="time"
-          id="time"
-          name="time"
-          value={formData.time}
-          onChange={handleChange}
-          className="mt-1 p-2 w-full border rounded-md"
-        />
-      </div>
-      <div className="mb-4">
-        <label htmlFor="message" className="block text-sm font-medium text-gray-600">
-          Message:
-        </label>
-        <textarea
-          id="message"
-          name="message"
-          value={formData.message}
-          onChange={handleChange}
-          className="mt-1 p-2 w-full border rounded-md"
-        />
-      </div>
-      <button type="submit" className="bg-gray-500 text-white p-2 rounded-md border-2  border-transparent hover:border-black hover:bg-white hover:text-black sm:text-center">
-        Submit
-      </button>
-    </form>
+    <div className='pt-10 pb-16' style={{ backgroundImage: "url('https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')" }}>
+      <form onSubmit={handleSubmit} className="max-w-md mx-auto p-4 border rounded bg-gray-50">
+        <div className="">
+          <div className='text-center mt-2 mb-8'>
+            <h1 className='text-md font-bold mb-1'>TABLE RESERVATION</h1>
+            <span className='text-md text-gray-500'>Make table reservation at QRAnbessa.</span>
+          </div>  
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            placeholder='Your name'
+            className="mt-1 p-2 w-full border rounded-md"
+          />
+        </div>
+        <div className="mb-4">
+          <input
+            type="text"
+            id="phone"
+            name="phone"
+            value={formData.phone}
+            onChange={handleChange}
+            placeholder='Phone'
+            className="mt-1 p-2 w-full border rounded-md"
+          />
+        </div>
+        <div className='flex gap-4'>
+          <div className="mb-4 w-full">
+            <input
+              type="date"
+              id="date"
+              name="date"
+              value={formData.date}
+              onChange={handleChange}
+              className="mt-1 p-2 w-full border rounded-md"
+            />
+          </div>
+          <div className="mb-4 w-full">
+            <input
+              type="time"
+              id="time"
+              name="time"
+              value={formData.time}
+              onChange={handleChange}
+              className="mt-1 p-2 w-full border rounded-md"
+            />
+          </div>
+        </div>
+        <div className="mb-4">
+          <textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            placeholder='Message'
+            className="mt-1 p-2 w-full border rounded-md"
+          />
+        </div>
+        <FormControl fullWidth margin="normal">
+            <CustomSelector
+              label="Select A Branch"
+              menuItems={[
+                { id: "option1", name: "HeaderQuarter" },
+                { id: "option2", name: "Castle" },
+              ]}
+              onChange={handleOptionChange}
+              value={selectedOption}
+              className="mt-1 p-2 w-full border rounded-md"
+            />
+          </FormControl>
+        <button type="submit" className="bg-gray-500 text-white p-2 rounded-md border-2  border-transparent hover:border-black hover:bg-white hover:text-black sm:text-center">
+          Submit
+        </button>
+      </form>
+    </div>
+    
   );
 };
 
