65dc45f727e6e6cb8d5f7370cf98a4f5b6f96a2f
Natan-Mesele <huluale12@gmail.com>
2024-01-22 11:00:32 +0300

    update

--- src/Scene/ItemCard.jsx
+++ src/Scene/ItemCard.jsx
@@ -110,7 +110,7 @@ function ItemCard() {
         </div>
       </div>
       <div className="">
-        <ul className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-4 m-4">
+        <ul className="grid grid-cols-2 sm:grid sm:grid-cols-2 lg:grid lg:grid-cols-3 gap-6 m-4">
           {jsonData.items.map((item, index) => (
             <li
               key={item.id}
